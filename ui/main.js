5519959a4a4fe8cc3b54bb7ea97ae847c6905b61
Karthikrajendran27 <30802706+Karthikrajendran27@users.noreply.github.com>
2017-08-13 22:23:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,12 @@ element.innerHTML = 'New Value';
 
 //Move the Image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight (){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function () {
     var interval = setInterval(moveRight, 100);
 };
