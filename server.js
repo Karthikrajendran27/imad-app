5ad659c79d3b6e43863160099b1e9709d92babf0
Karthikrajendran27 <30802706+Karthikrajendran27@users.noreply.github.com>
2017-08-14 21:47:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,9 @@ app.get('/:articleName', function (req, res) {
     res.send(createTemplate(articles[articleName]));
 });
 
-
+app.get('/favicon', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
