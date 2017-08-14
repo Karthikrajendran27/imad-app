9571c665aef492b7497729c18f0b34cc2ea45983
Karthikrajendran27 <30802706+Karthikrajendran27@users.noreply.github.com>
2017-08-14 22:21:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick = function () {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-            }
+            };
             
         }
         //Not done yet
