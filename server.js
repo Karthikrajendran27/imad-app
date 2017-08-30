8a19641f729171e4da57de3f60c64596b4df569a
Karthikrajendran27 <30802706+Karthikrajendran27@users.noreply.github.com>
2017-08-30 10:53:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = { 
     user: 'krthkraj6',
@@ -55,6 +56,21 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt) {
+    //Creating a Hash
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+    
+}
+
+
+
+
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req.params.input, 'This-is-some-Random-string');
+    res.send(hashedString);
 });
 
 
